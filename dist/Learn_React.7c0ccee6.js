995e158049f6d87cc52d5cf6add31d5668bc0c31
abhishek7199 <abhishek77882@gmail.com>
2025-07-24 15:36:57 +0530

    learn component,JSX,functional component, props

---
+++ dist/Learn_React.7c0ccee6.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("roots"));
+root.render(heading);
+
+//# sourceMappingURL=Learn_React.7c0ccee6.js.map
